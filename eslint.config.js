a309854220bd6b2cfa5c0efa41c3927093eec171
Мария Ширяева <mariya.shiryaeva@travelline.ru>
2025-08-18 14:14:52 +0300

    Настроить eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
+import globals from 'globals'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default ts.config([
+  globalIgnores(['dist', 'node_modules']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    extends: [
+      js.configs.recommended,
+      ts.configs.recommended,
+    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    extends: [
+      reactHooksPlugin.configs['recommended-latest'],
+      reactRefreshPlugin.configs.vite,
+    ],
   },
 ])
