31360bf21f525a32faec64eeda54ace68f29005b
Мария Ширяева <mariya.shiryaeva@travelline.ru>
2025-08-18 15:05:58 +0300

    Настроить stylelint при сборке.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import stylelint from 'vite-plugin-stylelint'
+
+const lintConfig = {
+  cache: false,
+  exclude: ['**/node_modules/**', '**/dist/**'],
+  lintOnStart: true,
+  emitWarning: true,
+  emitError: true,
+  failOnWarning: false,
+  failOnError: false,
+};
 
 export default defineConfig({
-  plugins: [react(), eslint({
-    include: 'src/**/*.{js,jsx,ts,tsx}',
-    exclude: ['**/node_modules/**', '**/dist/**'],
-    lintOnStart: true,
-    emitWarning: true,
-    emitError: true,
-    failOnWarning: false,
-    failOnError: false,
-  })],
+  plugins: [
+    react(),
+    eslint({
+      ...lintConfig,
+      include: 'src/**/*.{js,jsx,ts,tsx}',
+    }),
+    stylelint({
+      ...lintConfig,
+      include: ['src/**/*.css'],
+    }),
+  ],
 })
