558e5a9860c1ceadc440eb97ee56df78767c35b8
Мария Ширяева <mariya.shiryaeva@travelline.ru>
2025-08-21 09:35:05 +0300

    Добавить кнопку "назад".

--- src/pages/product-details/product-details.tsx
+++ src/pages/product-details/product-details.tsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 import { Flex, Typography } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
 import LayoutBase from '~/layouts/base/base';
 import { CATALOG } from '~/const/mock';
 import Product from '~/components/product/product';
+import { AppRoute } from '~/const/route/app-route';
 
 import styles from './product-details.module.css';
 
@@ -14,6 +16,11 @@ export default function ProductDetails() {
 
   return (
     <LayoutBase title="Детальная">
+      <Link to={AppRoute.Home} className={styles.backLink}>
+        <LeftOutlined className={styles.backIcon} />
+        <Text className={styles.backText}>Вернуться в каталог</Text>
+      </Link>
+
       {product ? (
         <Product data={product} />
       ) : (
