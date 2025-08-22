5807f3048e3271dee990904c7dc113d758695f08
Мария Ширяева <mariya.shiryaeva@travelline.ru>
2025-08-22 08:47:01 +0300

    Подключить форму.

--- src/pages/checkout/checkout.tsx
+++ src/pages/checkout/checkout.tsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router';
+import { Flex } from 'antd';
 import LayoutBase from '~/layouts/base/base';
-import { AppRoute } from '~/const/route/app-route';
+import OrderSuccess from '~/components/order-success/order-success';
+import OrderCard from '~/components/order-card/order-card';
+import OrderForm from '~/components/order-form/order-form';
+
+import styles from './checkout.module.css';
 
 export default function Checkout() {
+  const orderCompleted = false;
+
   return (
-    <LayoutBase title="Оформления заказа">
-      <div>Текст</div>
-      <Link to={AppRoute.Home}>Вернуться на главную</Link>
+    <LayoutBase title="Оформление заказа">
+      {orderCompleted ? (
+        <OrderSuccess />
+      ) : (
+        <Flex gap={24} align="start" className={styles.wrapper}>
+          <OrderForm />
+          <OrderCard />
+        </Flex>
+      )}
     </LayoutBase>
   );
 }
